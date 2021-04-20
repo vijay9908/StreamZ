00091f544a4e04533d3ed2c8cd0ad457be4f08b9
vijay9908 <vijaytanmay055@gmail.com>
2021-04-20 20:23:13 +0530

    playback and media storage support added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('',views.index_view,name='home'),
+    path('video/<int:video_id>/', views.watch_video, name='watch_video'),
     path('dashboard/', views.dashboard_view,name="dashboard"),
     path('login/',LoginView.as_view(),name="login_url"),
     path('register/',views.register_view,name="register"),
     path('logout/', LogoutView.as_view(next_page='dashboard'),name="logout_url"),
+    path('profile/', views.profile, name="profile"),
+    #path('<str:session_username>/profile/', views.profile,name="profile"), # for Recommendation Engine
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,15 +2,43 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Video, UserData
+from django.core.exceptions import ObjectDoesNotExist   # for further icing subhash
 
 # Create your views here.
 
 def index_view(request):
-    return render(request, 'index.html')
+    #return render(request,'index.html', context)
+    if not request.user.is_authenticated:
+        vids = Video.objects.all().order_by('-id')#[:5] # alpha testing - 5 vids available
+        context = { "videos" : vids}
+        return render(request,'index.html', context)
+    else:
+        vids = Video.objects.all().order_by('-id')
+        context = { "videos" : vids}
+        return render(request, 'dashboard.html', context)
+
+def watch_video(request, video_id):
+    try:
+        video_object = Video.objects.get(id=video_id)
+    except ObjectDoesNotExist:
+        return render(request, '404.html')
+    try:
+        session_object = User.objects.get(username = request.user.username)
+    except:
+        messages.warning(request, "Please Login to watch the video ") # left to Likith & mouni to display message or not
+        return redirect('index_view')
+    context = {'video': video_object}
+    return render(request, 'video.html', context)
 
 @login_required
 def dashboard_view(request):
-    return render(request, 'dashboard.html')
+    vids = Video.objects.all().order_by('-id')
+    context = { "videos" : vids}
+    return render(request, 'dashboard.html', context)
+    #return render(request, 'dashboard.html')
 
 def register_view(request):
     if request.method == 'POST':
@@ -27,5 +55,14 @@ def register_view(request):
 def login_view(request):
     return render(request, 'registration/login.html')
     #return render(request, 'login.html')
+
+def profile(request):  #session_username
+    try:
+        session_obj = User.objects.get(username = request.user.username)
+    except ObjectDoesNotExist:
+        return render(request, '404.html')
+    profile_data = UserData.objects.get_or_create(user=session_obj)[0]
+    context = {'session_obj': session_obj, 'user_data': profile_data}
+    return render(request, 'profile.html', context)   #profile stuff - likith handle this.
 
 

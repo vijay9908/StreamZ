8ac1a713c8155828fc898ba78529e76c971507d4
vijay9908 <vijaytanmay055@gmail.com>
2021-04-21 14:28:00 +0530

    fixed video routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('register/',views.register_view,name="register"),
     path('logout/', LogoutView.as_view(next_page='dashboard'),name="logout_url"),
     path('profile/', views.profile, name="profile"),
-    #path('<str:session_username>/profile/', views.profile,name="profile"), # for Recommendation Engine
+    #path('<str:session_username>/profile/', views.profile,name="profile"), # for Recommendation Engine @subhash
 ]

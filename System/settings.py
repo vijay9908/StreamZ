e7ad9e8e36f1a83f95a5a01ed356f67398dd3e77
vijay9908 <vijaytanmay055@gmail.com>
2021-05-15 19:19:06 +0530

    hosts allowed

--- System/settings.py
+++ System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wwkb5y2m0t+74@oda4=gq=wsq9!w3byvu^2-)0#+y$ic0!*-mg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
